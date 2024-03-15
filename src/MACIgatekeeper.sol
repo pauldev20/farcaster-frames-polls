df4bfc70081b489c8ba6bfcef33568ef03c45261
Patrick Fuchs <email@patrick-fuchs.com>
2024-03-15 21:33:54 +0000

    add SingUpGatekeeper

--- src/MACIgatekeeper.sol
+++ src/MACIgatekeeper.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IWorldID} from "world-id-onchain-template/contracts/src/interfaces/IWorldID.sol";
+import {IWorldID} from "../lib/world-id-onchain-template/contracts/src/interfaces/IWorldID.sol";
+import {SignUpGatekeeper} from "../lib/maci/contracts/contracts/gatekeepers/SignUpTokenGatekeeper.sol";
 
-contract MACIGatekeeper {
+contract MACIGatekeeper is SignUpGatekeeper{
 
     function decodeData(bytes memory data) internal pure returns (address, uint256, uint256, uint256[8] memory) {
         (address signal, uint256 merkle_root, uint256 nullifierHash, uint256[8] memory proof) = abi.decode(data, (address, uint256, uint256, uint256[8]));
