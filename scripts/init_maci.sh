d1df74004b54ad8a6d066eaf078650b0f951e6d7
lorbke <luca.orbke@brinkjost.de>
2024-03-16 15:28:09 +0000

    pass gatekeeper address as argument

--- scripts/init_maci.sh
+++ scripts/init_maci.sh
@@ -1,6 +1,14 @@
 BLUE='\e[0;34m'
 RESET='\e[0m'
 
+# gatekeeper=0xDd34A8E7345E7D398Fc618421AF57c6812b76bBd
+gatekeeper=$1
+
+if [ -z "$1" ]; then
+    echo "Missing gatekeeper parameter. usage: sh init_maci.sh [gatekeeper_address]"
+    exit
+fi
+
 CLI_DIR='../../votelik/maci/cli'
 cd $CLI_DIR
 
@@ -19,4 +27,4 @@ node build/ts/index.js setVerifyingKeys \
 
 echo "$BLUE create MACI $RESET"
 node build/ts/index.js create -s 10 \
-    --signupGatekeeperAddress 0xDd34A8E7345E7D398Fc618421AF57c6812b76bBd
+    --signupGatekeeperAddress $gatekeeper
