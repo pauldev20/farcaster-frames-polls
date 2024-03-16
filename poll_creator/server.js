ed09214adaa5df91017282bf377b90a664bbe10c
Patrick Fuchs <email@patrick-fuchs.com>
2024-03-16 13:28:30 +0000

    fix poll creator use cli

--- poll_creator/server.js
+++ poll_creator/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { exec } = require('child_process');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -32,6 +33,21 @@ app.post('/create', (req, res) => {
     // Store poll in memory
     polls.push(poll);
 
+    exec('node ../maci/cli/build/ts/index.js deployPoll \
+    -pk macipk.78e716652dfd1cc8f5fb0b45f656b493135edba5447cfc5e68c2f568d22e7193 \
+    -t 300 -i 1 -m 2 -b 1 -v ' + options.length, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+    options.length
     res.status(201).json({ message: 'Poll created successfully' });
 });
 
