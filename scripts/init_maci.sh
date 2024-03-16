077788252d69cbe7e008ea2e69132d9b714fef51
lorbke <luca.orbke@brinkjost.de>
2024-03-16 21:03:16 +0000

    remove double creation of vkRegistry

--- scripts/init_maci.sh
+++ scripts/init_maci.sh
@@ -12,18 +12,20 @@ fi
 CLI_DIR='../../votelik/maci/cli'
 cd $CLI_DIR
 
-echo "$BLUE DeployingVkRegistry $RESET"
-node build/ts/index.js deployVkRegistry
+# echo "$BLUE DeployingVkRegistry $RESET"
+# node build/ts/index.js deployVkRegistry
+    # --rpc-provider https://sepolia.drpc.org
 
-echo "$BLUE setVerifyingKeys $RESET"
-node build/ts/index.js setVerifyingKeys \
-    --state-tree-depth 10 \
-    --int-state-tree-depth 1 \
-    --msg-tree-depth 2 \
-    --vote-option-tree-depth 2 \
-    --msg-batch-depth 1 \
-    --process-messages-zkey ./zkeys/ProcessMessages_10-2-1-2_test/ProcessMessages_10-2-1-2_test.0.zkey \
-    --tally-votes-zkey ./zkeys/TallyVotes_10-1-2_test/TallyVotes_10-1-2_test.0.zkey
+# echo "$BLUE setVerifyingKeys $RESET"
+# node build/ts/index.js setVerifyingKeys \
+#     --state-tree-depth 10 \
+#     --int-state-tree-depth 1 \
+#     --msg-tree-depth 2 \
+#     --vote-option-tree-depth 2 \
+#     --msg-batch-depth 1 \
+#     --vk-registry 0x7bB04d019D768f47dDe5CA0D78c9Fbd011F02788 \
+#     --process-messages-zkey ./zkeys/ProcessMessages_10-2-1-2_test/ProcessMessages_10-2-1-2_test.0.zkey \
+#     --tally-votes-zkey ./zkeys/TallyVotes_10-1-2_test/TallyVotes_10-1-2_test.0.zkey
 
 echo "$BLUE create MACI $RESET"
 node build/ts/index.js create -s 10 \
